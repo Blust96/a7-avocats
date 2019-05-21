ee3d52b6f2059eb5fda9cbf9a185c30a91acfd9b
Quentin <q.bohain@gmail.com>
2019-05-21 17:10:07 +0200

    Creating conversation id generation, updating bot fetch to async/await style

--- src/services/chatbot.js
+++ src/services/chatbot.js
@@ -7,38 +7,45 @@ const request_token = 'b580b2fc58b71aa3de4c82fd6b66861a';
 /*
 Service definition
 */
-const sendBotRequest = (message_content, conversation_id, language) => {
-    
-    return new Promise((resolve, reject) => {
+const generateConversationId = (id_length) => {
 
-        fetch('https://api.cai.tools.sap/build/v1/dialog', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Token ${request_token}`
+    let conversation_id = '';
+    const id_characters = 'ABCDEFGHIJKLM0123456789NOPQRSTUVWXYZ01234abcdefghijklm56789nopqrstuvwxyz0123456789';
+    const id_characters_length = id_characters.length;
+
+    for(let i = 0 ; i < id_length ; i++) {
+        conversation_id += id_characters.charAt(Math.floor(Math.random() * id_characters_length));
+    }
+
+    return conversation_id;
+
+}
+
+const sendBotRequest = async (message_content, conversation_id, language) => {
+
+    let response = await fetch('https://api.cai.tools.sap/build/v1/dialog', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${request_token}`
+        },
+        body: JSON.stringify({
+            message: {
+                type: 'text',
+                content: message_content
             },
-            body: JSON.stringify({
-                message: {
-                    type: 'text',
-                    content: message_content
-                },
-                conversation_id: conversation_id,
-                language: language
-            })
+            conversation_id: conversation_id,
+            language: language
         })
-        .then(res => res.json())
-        .then(json => {
-            if(json.results !== null) {
-                resolve(json.results.messages);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            reject(error);
-        })
-
     });
+
+    let json = await response.json();
+
+    if(json.results !== null)
+        return json.results.messages[0];
+
+    return null;
 
 }
 //
@@ -47,6 +54,7 @@ const sendBotRequest = (message_content, conversation_id, language) => {
 Export
 */
 module.exports = {
+    generateConversationId,
     sendBotRequest,
 }
 //
